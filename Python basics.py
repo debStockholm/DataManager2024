f8bd7b901bb73e89a4938177615a68ea3e2c290c
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2024-12-17 13:34:34 +0100

    ovningar

--- Python basics.py
+++ Python basics.py
@@ -1,5 +1,4 @@
-
-
+#-------------               hamta datatyp
 print(56+9+79)
 exem1 = ('exempel programmering')
 print(type(exem1))
@@ -8,22 +7,10 @@ print(type(exem1))
 x=10
 y=20
 
-summa= x + y
-print(summa) 
-
-lo_u =89
-print(lo_u)
-
-
-x=42
-y = str(x)
-
-print(type(y))
-
+#----------------------     github commit push
 exempel_GITHUB = 'denna fil'
 xh = 'exempel1'
 xy = 673
-
 
 variabel1 ='123'
 print(variabel1)
@@ -31,15 +18,15 @@ print(variabel1)
 y=2
 print(int(variabel1)*y)
 
-#numbers i Python
+#-----------------------   numbers i Python
 exempelsiffra = 98.08
-exempelsiffra_ny=(int(exempelsiffra))
+exempelsiffra_ny=int(exempelsiffra)
 print(exempelsiffra_ny)
 
-#---------
+#-------------------------- mathematical operations
 
 a = 3 + 2
-b = 26//3   #'hur manga ggr kan delas med jamnt nummer'
+b = 26//3   #'hur manga ggr kan delas med hela nummer'
 b1 =28//3 
 b2 =27//3
 c = 5 % 2   #resten vid division
@@ -51,7 +38,52 @@ print(a)
 print(b)
 print(b1)
 print(b2)
-
 print(c)
 print(d)
 print(e)
+
+#---------------------- math operations 2
+x=10
+y=3
+
+
+print(x / y)
+print(x//y)   #'hur manga ggr kan delas med jamnt nummer'
+print(x % y)   #resten vid division
+print(x ** y)  #uppe till ---
+
+#-------------------------- 
+
+pi = 3.14
+y=int(pi)
+print(y)  #convert pi till heltal
+
+int5 = 5
+int5dec =float(int5)
+print(int5dec)    #convert heltal till flyttal
+
+#------------------- avrundning
+
+ui= 0.1 + 0.2
+print(ui)
+
+print(round(ui , 4))
+
+tal1 = 0.1
+tal2 = 0.2
+
+tal1 = int(tal1 * 10)
+tal2 = int(tal2 * 10)
+print(tal1 + tal2)
+
+avrundat= (tal1 + tal2)/10
+print(avrundat)
+#-------------------------
+
+x = '15'
+y = '5'
+print(type(y))
+x1 =int(x)
+print(type(x1))
+summa = int(x) + int(y)
+print(f'summan ar {summa}')
