215ef96fca23d38bf28738d218feaa4c200eb2c7
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2024-12-17 10:31:18 +0100

    i mina andringar

--- Python basics.py
+++ Python basics.py
@@ -1,2 +1,22 @@
-print("hello")
+
+
+print(56+9+79)
+exem1 = ('exempel programmering')
+print(type(exem1))
+
+
+x=10
+y=20
+
+summa= x + y
+print(summa) 
+
+lo_u =89
+print(lo_u)
+
+
+x=42
+y = str(x)
+
+print(type(y))
 
---
+++ commit_exempel.py
@@ -0,0 +1,27 @@
+print("hello, skapa ny mapp")
+
+#git status
+#git add  -A
+#git commit -and 'meddelande' -- valfritt meddelande
+#git pull origin  main/master
+#git push origin main/master
+
+print('hello world')
+xd=9
+if xd==8:
+  print('4 is less than');
+elif xd<0:
+  print('4 is bigger than');
+else:
+  print('elseprint');
+
+
+
+
+
+
+x=10
+y=20
+
+summa= x + y
+print(summa)
