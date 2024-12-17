6592419ddba16d3b0eb5cef23318258c48b47074
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2024-12-17 10:52:09 +0100

    trying saving new

---
+++ instructionGIT.py/instructionGIT.py
@@ -0,0 +1,9 @@
+#check GIT status
+
+#om filen existerar, direct till git commit -am , annars 'git add'
+#git commit -am + "fritt meddelande"
+#git push -- set-upstream origin master  ---> enter
+
+instruction = 'instruction test'
+save = 'variable save
+'
