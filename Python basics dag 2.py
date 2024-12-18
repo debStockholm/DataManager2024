7cc718861c50c663b02221f9945824cddbdc9f0e
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2024-12-18 09:29:07 +0100

    dagens

---
+++ Python basics dag 2.py
@@ -0,0 +1,16 @@
+print("hello, skapa ny mapp")
+
+#git status
+#git add  -A
+#git commit -and 'meddelande' -- valfritt meddelande   'git commit -am' + 'valfritt meddelande'
+#git pull origin  main/master
+#git push origin main/master
+
+
+ #----------TRY/EXCEPT for felsokning
+
+try: 
+  x =int('exempel text')
+  print(x)
+except:
+  print('funkar ej')
