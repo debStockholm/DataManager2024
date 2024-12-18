89c0c83c59ef75ba2ff0ef49632a54acf71828ca
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2024-12-18 14:15:17 +0100

    efermiddag

--- Python basics dag 2.py
+++ Python basics dag 2.py
@@ -57,6 +57,9 @@ print(type(Min_lista2))
 
         #skriv ut varje tredje item
 reallyLongList = ["äpple", "banan", "körsbär", "druva", "apelsin", "päron", "kiwi", "mango", "passionsfrukt", "ananas"]
-for frukt in reallyLongList:
-    if frukt == [2,5,8,11,14,17,20]:
-      print(frukt)
+
+for index, number in enumerate(reallyLongList):
+    print(index,number)
+
+
+
