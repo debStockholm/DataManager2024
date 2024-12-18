07d576fac7fd8a7e2c1812ea1e73f384bcc768fb
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2024-12-18 10:38:38 +0100

    update1

--- Python basics dag 2.py
+++ Python basics dag 2.py
@@ -7,10 +7,25 @@ print("hello, skapa ny mapp")
 #git push origin main/master
 
 
- #----------TRY/EXCEPT for felsokning
+ #----------TRY/EXCEPT for felsokning  + FINALLY
 
 try: 
-  x =int('exempel text')
+  x =str('hej')   # om TRY kod funkar: returneras TRY Kod i terminal
   print(x)
+
 except:
-  print('funkar ej')
+  print('funkar ej') #om TRY kod INTE fungerar: returneras EXCEPT kod
+
+ 
+
+  #--------------LISTOR
+
+  #En insamling av flera varde av samma typ. Alla varden har specifika position och kan anropas genom det
+
+Min_lista=(1, 4, 6 , 8, 9, 13)
+print(Min_lista[3])   # exempel varde hamtat fran en lista
+print(Min_lista[-1])   # -1 hamtar sista element av en lista eller range
+
+Min_lista=(1, 4, 'textexempel', 6 , 'text11' , 8, 9, 13)
+print(Min_lista[-2])   # -2 tecket hamtar varden fran en lista fran den sista till den forsta
+print(Min_lista[2])
