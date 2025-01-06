7b88486c549ff72e1a8ea637f25a5060b4170acc
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-06 20:06:40 +0100

    Pycharm exercise file

--- DataScience python programming.py
+++ DataScience python programming.py
@@ -1,6 +1,36 @@
-print("hello")
+'''print("hello")
 print(6)
 print('trying to commit')
 
 o_mit_ = 'exempel GITHUB'
-print(o_mit_)
+print(o_mit_)'''
+
+
+'''List_uppgift5 = [0, 17, 18, 19, 20, 21, 34, 37, 51, 54]
+for x in List_uppgift5:
+    if x%2 == 1:
+        print(x)'''
+
+'''a = 0
+b = 1
+for c in range(10):
+   c = a + b
+   print(c)
+   a = b
+   b = c
+
+a = 0
+b = 1
+for x in range(10):
+   x = a + b
+   print(x)
+   a = b
+   b = x
+   '''
+Losenord_list = [input(str)]
+if len(Losenord_list) < 8:
+     print('Losenord behover minst 8 tecken')
+elif len(Losenord_list) == 8 and Losenord_list == str:
+     print('need a number')
+else:
+      print('losenord accepterat')
