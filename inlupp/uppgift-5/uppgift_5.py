77bff70e01d6a5e9a4b743efa6b255abb7b77071
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-07 20:28:17 +0100

    uppgift 6

--- inlupp/uppgift-5/uppgift_5.py
+++ inlupp/uppgift-5/uppgift_5.py
@@ -2,13 +2,13 @@
 # Skapa en funktion filter_odd(numbers) som returnerar en lista med alla jämna tal från den givna listan.
 
 def filter_odd(list_numbers:list):
+   ny_lista=[]
    for x in list_numbers:
-    x = int(list_numbers)
     if x%2 == 0:
        return x
     else:
        return 'odd number'
     
-print(filter_odd(6))
+print(filter_odd(ny_lista([9, 0, 8, 12, 91, 67, 47, 42, 5])))
 
     
--- inlupp/uppgift-6/uppgift_6.py
+++ inlupp/uppgift-6/uppgift_6.py
@@ -1,8 +1,9 @@
 # Uppgift 6
 # Skapa en funktion multiplication_table(n, limit) som returnerar multiplikationstabellen för n upp till limit i en lista.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
+def multiplication_table(i:list):
+ for i in range(15):
+      print(i, i*6)
+ 
+print(multiplication_table(6))  #ar det mitt misstag?
+
