07c511f9ec0826a7114260c557a9efe290dce9e9
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-08 20:29:28 +0100

    fixa

--- inlupp/uppgift-6/uppgift_6.py
+++ inlupp/uppgift-6/uppgift_6.py
@@ -1,9 +1,10 @@
 # Uppgift 6
 # Skapa en funktion multiplication_table(n, limit) som returnerar multiplikationstabellen för n upp till limit i en lista.
 
-def multiplication_table(i:list):
- for i in range(15):
-      print(i, i*6)
+def multiplication_table(x:int, list2:list):  
+  for x in range(list2):  #x = termine (int) da moltiplicare, list2 dovrebbe essere il range in cui moltiplicarlo..?
+                          #come fare?
+     return x
  
-print(multiplication_table(6))  #ar det mitt misstag?
+print(multiplication_table(3,4)) #x= int e list2: limite max della moltiplicazione
 
