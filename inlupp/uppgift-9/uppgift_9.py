586d074e5b224d943ffe48c0cf478f1586170576
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-09 17:55:46 +0100

    fix

--- inlupp/uppgift-9/uppgift_9.py
+++ inlupp/uppgift-9/uppgift_9.py
@@ -2,9 +2,10 @@
 # Skapa en funktion is_palindrome(string) som kontrollerar om en given sträng är ett palindrom (dvs. samma framifrån och bakifrån).
 
 def is_palindrome(x:str):
-    x = input('skriv ett valfritt ord:')    #slice?
+    #x = input('skriv ett valfritt ord:')    #slice?
     if (x == x[::-1]):
-            return 'is palindrome'       #funkar ej riktigt
-    else:
-            return 'is NOT pal'
+            return True                  #input fuction doesnt work? #step 1
+    else:                                #altri modi per risolvere?
+            return False
 
+print(is_palindrome(''))     #il programma funziona, ma risultato : 'test failed?'
