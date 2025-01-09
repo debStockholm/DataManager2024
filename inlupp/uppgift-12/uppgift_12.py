1cd932bd4cb5803251fbc66dcdc9ccd8f86343ee
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-09 19:01:02 +0100

    fixa!

--- inlupp/uppgift-12/uppgift_12.py
+++ inlupp/uppgift-12/uppgift_12.py
@@ -1,8 +1,19 @@
 # Uppgift 12
 # Skapa en funktion create_student_register(students) som tar emot en lista med namn och ålder och returnerar en dictionary där namnet är nyckeln och åldern är värdet.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
+def create_student_register(students:list):     
+    #Student_list = students.index()   #qualcosa tipo 'slice' che funziona per le liste? che soluzioni abbiamo?
+    Ny_Stud_list= {}
+
+    for namn, alder in students:               
+       return Ny_Stud_list.update              #come faccio ad ssicurarmi che le variabili raccolgano il valore esatto dall lista? 
+                                               # namn = un nome, alder = eta 
+
+print(create_student_register([('anna', 17), ('Emma', 16)]))
+
+
+'''procedura:
+creare  nuovo dict -> Student_list = {} a cui aggiungere dati da una lista
+
+devo usare slice? per dividere elementi in una lista?'''
+
--- inlupp/uppgift-4/uppgift_4.py
+++ inlupp/uppgift-4/uppgift_4.py
@@ -2,4 +2,13 @@
 # Skapa en funktion fibonacci(n) som returnerar en lista med de första n Fibonacci-talen.
 
 def test_fibonacci(exempel_list:list):
-  
+   a = 0                                   #help
+   b = 1
+for x in range(5):  
+      x = a + b
+      print(x)
+     a = 0
+     b = 1
+
+print(test_fibonacci([0, 1, 1, 2, 3]))
+   
