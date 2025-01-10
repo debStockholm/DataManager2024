78007ca01e51d55dedc2af0a7be50e8475ccd138
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-10 12:06:37 +0100

    test_ok

--- inlupp/uppgift-12/uppgift_12.py
+++ inlupp/uppgift-12/uppgift_12.py
@@ -2,11 +2,13 @@
 # Skapa en funktion create_student_register(students) som tar emot en lista med namn och ålder och returnerar en dictionary där namnet är nyckeln och åldern är värdet.
 
 def create_student_register(students:list):     
-    #Student_list = students.index()   #qualcosa tipo 'slice' che funziona per le liste? che soluzioni abbiamo?
-    Ny_Stud_list= {}
+    #Student_list = students.index()   
+    ny_Stud_dict = {}
 
     for namn, alder in students:               
-       return Ny_Stud_list.update              #come faccio ad ssicurarmi che le variabili raccolgano il valore esatto dall lista? 
+       ny_Stud_dict[namn]=alder
+    return ny_Stud_dict
+       #come faccio ad assicurarmi che le variabili raccolgano il valore esatto dall lista? 
                                                # namn = un nome, alder = eta 
 
 print(create_student_register([('anna', 17), ('Emma', 16)]))
