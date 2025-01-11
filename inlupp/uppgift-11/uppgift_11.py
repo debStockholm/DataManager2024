f9fd218ebdea3bbfdbc0c9d18f529f6e8655a090
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-11 14:12:57 +0100

    testat och klar

--- inlupp/uppgift-11/uppgift_11.py
+++ inlupp/uppgift-11/uppgift_11.py
@@ -2,13 +2,7 @@
 # Skapa en funktion word_count(text) som returnerar antalet ord i en given text.
 
 def word_count(text_ex:str):
-   split_line = text_ex.split("o w")   #altri metodi?    #1 step: dividere string in parole con split
-   return len(split_line)
+   split_line = text_ex.split()   #man delar den angivne string i ord genom split(). default: str delas mellan whitespaces
+   return len(split_line)         #returnerar antalet ord da len raknar ord texten blev uppdelat i
    
-print(word_count('hello worlll fdde'))  #funkar
 
-'''def word_count2(ex2:str):
-   string_divided = ex2.split()
-   print(len(string_divided))
-
-print(word_count2('provar detta om det fungerar')) #perché risulta anche 'none'?'''
