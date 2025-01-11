02a907375fbab4d7ed66bad95ac1c0922d6b1698
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-11 12:42:35 +0100

    testat och klar

--- inlupp/uppgift-5/uppgift_5.py
+++ inlupp/uppgift-5/uppgift_5.py
@@ -1,5 +1,7 @@
 # Uppgift 5
 # Skapa en funktion filter_odd(numbers) som returnerar en lista med alla jämna tal från den givna listan.
+#forst skapas en variabel som kommer innehalla resultatet av for loopen. Resultat sparas i listan genom append() metod.
+#till slut, returneras den nyskapade lista med udda siffror.
 
 def filter_odd(list_numbers:list):
    new_list = []
