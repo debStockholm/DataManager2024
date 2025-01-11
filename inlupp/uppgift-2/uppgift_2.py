f43307a3d6c2108c02b4ee0ab7295a6d8f43db6d
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-11 12:39:54 +0100

    testat och klar

--- inlupp/uppgift-2/uppgift_2.py
+++ inlupp/uppgift-2/uppgift_2.py
@@ -1,5 +1,6 @@
 # Uppgift 2
 # Skapa en funktion sum_list(numbers) som returnerar summan av alla siffror i listan.
+# Enklast sum operator som adderar alla varde i en lista
 
 def sum_list(ls:list):
     return (sum(ls))
