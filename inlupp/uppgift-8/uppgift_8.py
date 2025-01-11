a3b5e9e72a7e378a7b3b1e16c7f720e744c0a025
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-11 14:03:51 +0100

    testat och klar

--- inlupp/uppgift-8/uppgift_8.py
+++ inlupp/uppgift-8/uppgift_8.py
@@ -5,8 +5,8 @@
 def count_letters(x:str):                      #dictionary: {'A':'8', 'B': '6',}
     dictionary_upp = {}                        #1 step: skapa en variabel som innehaller den nya dict {dictionary_upp} 
     for bokstav in x:                          #tillaga varde i en: {} dict['bokstav']= 'x'
-        if bokstav not in dictionary_upp:        #2 step: kora il else satsen: 
-         dictionary_upp[bokstav] = 1           #om en bokstav ej fanns i dict: tillags
+        if bokstav not in dictionary_upp:        #2 step: kora if/else satsen: 
+         dictionary_upp[bokstav] = 1           #om en bokstav ej redan finns i dict: tillags
         else: 
          dictionary_upp[bokstav] += 1                   #om en boksav finns i dict: count + 1
     return dictionary_upp
