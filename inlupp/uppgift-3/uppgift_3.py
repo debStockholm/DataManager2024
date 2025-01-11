fd82230c4c3a6d905e02c80a04e62780f6f6f15c
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-11 12:40:49 +0100

    testat och klar

--- inlupp/uppgift-3/uppgift_3.py
+++ inlupp/uppgift-3/uppgift_3.py
@@ -1,7 +1,6 @@
 # Uppgift 3
 # Hitta det största talet i en lista
-
+#Enklast max operator, som sorterar ut den hogsta talet i en lista
 def max_in_list(maxtal:int):
     return (max(maxtal))
 
-#funkar, men kan ej testa
