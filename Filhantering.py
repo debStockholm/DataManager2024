e45bb200fc1eb81698eaa1164a4c42c643f27694
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-13 11:31:57 +0100

    testat och klar

--- Filhantering.py
+++ Filhantering.py
@@ -3,14 +3,19 @@
 #---------------------------------FILHANTERING----------------------------------
 #-------TEXT--------------------------
 '''with open('data_txt', 'w') as file:
-    file.write('Name, Age, City\nSofia, 25, Stockholm\n\nBjorn, 34, Goteborg\n')
+    file.write('Name, Age, City\nSofia, 25, Stockholm\n \nBjorn, 34, Goteborg\n')'''
 
 
 with open('data_txt', 'r') as file:
-    print(file.read())'''
+    print(file.read())
 
-#---------------CSV------------------------------------
-'''with open("data_exempel.csv", "w") as file_exempel:
+    for line_ex in file:
+      print(line_ex.strip())  #ta bort whitespaces, ma boh, mica funziona? csv reader lo fa per me automaticamente
+
+    
+    '''
+#----------------------------------------------------CSV--------------------------------------------
+with open("data_exempel.csv", "w") as file_exempel:
  
  file_exempel.write('Product, Price, Stock\nBanana, 3, 200\nApple, 5, 100\nOrange, 4, 150')
  print(file_exempel)  #---> skrev inne i file_exempel.cvs, kolla!
@@ -18,18 +23,18 @@ with open('data_txt', 'r') as file:
 
  import csv  #----> ska alltid vara med!
 
-with open('data_exempel.csv', 'r') as file_reader_ex:
-    lines_in_file = csv.reader(file_reader_ex)
-    for rows in lines_in_file:
+with open('data_exempel.csv', 'r') as file:      
+    lines_in_file = csv.reader(file)
+    for rows in file:
        print(rows)
 
-with open('data_exempel.csv', 'r') as file_reader_ex:
-   add_category = csv.reader(file_reader_ex)
+with open('data_exempel.csv', 'r') as file:
+   add_category = csv.reader(file)
    new_category = list(add_category) #detta gor att data blir en list type!
 
-new_category[0]= new_category.append('CAtegory')
-print(file_reader_ex)
+new_category[0]= new_category.append('app_category')
+print(file)'''
 
 
-'''
+
 
