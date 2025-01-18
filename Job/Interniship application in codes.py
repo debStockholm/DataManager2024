5ef29080d99dc6f1b909a1a2b08a23707f408998
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-01-18 02:04:34 +0100

    Job/internship app up

--- Job/Interniship application in codes.py
+++ Job/Interniship application in codes.py
@@ -9,9 +9,31 @@ work_extra_or_and_summer = 'Can work part-time/hourly during the whole 2025'
 print(work_extra_or_and_summer)
 
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+Best_candidate = {
+    'Skills': ['Python', 'SQL', 'ProjectManagement' , 'Humor', 'ChatGPT', 'GitHub', 'Rail infrastructure designing', 'Brings sweets to office', 'Mind her own business' ],
+    'On a scale from 1 to 10': [3, 5,-5, 10, 8,-2, 7, 20, 9]
+}
+
+best_cand=pd.DataFrame(Best_candidate)
+best_cand.to_csv('best candidate application.csv', index = False)
+
+
+Best_best= pd.read_csv('best candidate application.csv')
+Best_of_the_best = Best_best.sort_values(by= 'On a scale from 1 to 10', ascending =True)
+print(Best_of_the_best)
+
+sns.barplot(data= Best_of_the_best, x='Skills', y = 'On a scale from 1 to 10')
+plt.title('Debora the best candidate:')
+plt.show()
+#close Plot to keep on running code:
+
 print("...company team scanning available roles...loadin'...")
 
-work_position = 'available'   #please do not change
+work_position = 'available'  #please do not change
 deb = 'this candidate'
 
 if work_position == 'available':
@@ -23,7 +45,7 @@ elif work_position == 'not available':
     else: 
         print(f"can't miss this outstanding opportunity though, let's look better for {deb}' possibilities!")
 else:
-    print( 'do we really wanna miss this once in a lifetime opportunity? :-( ')
+    print( 'do we really wanna miss this once in a lifetime opportunity? :-( ')  #please don't choose :(
 
 
 print(f'{deb} reply back:')
@@ -37,30 +59,11 @@ for greeting in range(3):
     else:
         print('wish you a proficuous week!')
 
+
+
+print('Do you think my code is terrible? You should see my PM skills then! Reach me out if you wanna know more, or show me how to do best! :-)')
 #OBS
 print(f'({deb} has some humor)')
 
 
-
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-Best_candidate = {
-    'Skills': ['Python', 'SQL', 'ProjectManagement' , 'Humor', 'ChatGPT', 'GitHub', 'Rail infrastructure designing', 'Brings sweets to office', 'Mind her own business' ],
-    'On a scale from 1 (lower) to 10': [3,5,-6, 10, 8, -2, 7, 20, 9]
-}
-
-best_cand=pd.DataFrame(Best_candidate)
-best_cand.to_csv('best candidate application.csv', index = False)
-
-
-Best_best= pd.read_csv('best candidate application.csv')
-Best_of_the_best = Best_best.sort_values(by= 'On a scale from 1 (lower) to 10', ascending =True)
-print(Best_of_the_best)
-
-sns.barplot(data= Best_of_the_best, x='Skills', y = 'On a scale from 1 (lower) to 10')
-plt.title('Debora the best candidate:')
-plt.show()
-
-print('Do you think my code is terrible? You should see my PM skills then! Reach me out if you wanna know more, or show me how to do best! :-)')
+#committed and pushed
